ad1e2dfdef207e03c2d99eb0b50ce9a1395615b1
jagdeep sidhu <sidhujag@syscoin.org>
2023-07-12 11:03:46 -0700

    Update config.go

--- params/config.go
+++ params/config.go
@@ -201,7 +201,7 @@ var (
 		LondonBlock:                   big.NewInt(0),
 		ArrowGlacierBlock:             big.NewInt(0),
 		GrayGlacierBlock:              big.NewInt(0),
-		ShanghaiTime:                  newUint64(0),
+		ShanghaiTime:                  big.NewInt(0),
 		TerminalTotalDifficulty:       big.NewInt(0),
 		TerminalTotalDifficultyPassed: true,
 		IsDevMode:                     true,
