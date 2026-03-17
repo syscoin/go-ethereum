177d0329f099cec49813c840ba1adb5b55161a97
jagdeep sidhu <sidhujag@syscoin.org>
2026-03-16 17:36:51 -0700

    update to v5.1.0

--- version/version.go
+++ version/version.go
@@ -18,7 +18,7 @@ package version
 
 const (
 	Major = 5          // Major version component of the current release
-	Minor = 0         // Minor version component of the current release
-	Patch = 3        // Patch version component of the current release
+	Minor = 1         // Minor version component of the current release
+	Patch = 0        // Patch version component of the current release
 	Meta  = "stable" // Version metadata to append to the version string
 )
